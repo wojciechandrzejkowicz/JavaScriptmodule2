fbadee1d835f8b87ed35c45ac2140ffe9e9feabe
Wojciech Andrzejkowicz <wojciech.andrzejkowicz@wp.pl>
2017-07-27 18:35:25 +0200

    removing magic number

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
-var dinosaur = 'triceratops'
+var text = 'Velociraptor is a genus of herbivorous';
+var dinosaur = 'triceratops';
 var upperDinosaur = dinosaur.toUpperCase();
-
-console.log(upperDinosaur);
-
-var textAfter = text.replace('Velociraptor', upperDinosaur);
-
-console.log(textAfter);
-
-console.log(textAfter.length/2);
-
-var partOfText = textAfter.slice(0,72);
+var textArray = text.split(' ');
+var textAfter = text.replace(textArray[0], upperDinosaur);
+var partOfText = textAfter.slice(0, textAfter.length/2);
 
 console.log(partOfText);
