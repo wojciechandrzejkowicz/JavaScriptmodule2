87d1d53fddc511bcbc489822acd8342924f2ca2a
Wojciech Andrzejkowicz <wojciech.andrzejkowicz@wp.pl>
2017-07-27 09:29:02 +0200

    loop

---
+++ script.js
@@ -0,0 +1,9 @@
+function drawTree(x) {
+    for (var i = 0; i < x; i++) {
+        var star = '';
+        for (var j = 0; j <= i; j++) {
+           var star = star += '*';
+        } console.log(star)
+    }
+}
+drawTree(5)
