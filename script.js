7707c2001ea8758d807608e238aa75e4b275a760
Wojciech Andrzejkowicz <wojciech.andrzejkowicz@wp.pl>
2017-07-27 09:18:18 +0200

    tables

---
+++ script.js
@@ -0,0 +1,17 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var arr = femaleNames.concat(maleNames);
+
+console.log(arr);
+
+var newName = 'Marian';
+
+if (arr.indexOf(newName) > -1) {
+    console.log('imie w tablicy');
+} else {
+    console.log('brak imienia');
+}
+
+var x = arr.push(newName);
+
+console.log(x);
